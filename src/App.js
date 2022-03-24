af74f815b3e87b6e337124703519a9a9fced7509
github username <um_2003_8@hotmail.com>
2022-03-24 08:53:17 -0400

    creat a form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ class App extends Component {
     render() {
         return (
         <div>
-            
+            <form>
+                <input type="text" id="username" value={this.state.username} onChange={this.handleChange} />
+                <input type="text" id="password" value={this.state.password} onChange={this.handleChange} />
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
         )
     }
