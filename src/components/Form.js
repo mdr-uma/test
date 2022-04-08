bb823af51e69bbb9a4ddd87efe353c8118fd5cb6
github username <um_2003_8@hotmail.com>
2022-04-08 11:46:49 -0400

    add form input

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Form = () => {
     return (
         <div>
-            
+            <form>
+                <input></input>
+            </form>
         </div>
     )
 }
